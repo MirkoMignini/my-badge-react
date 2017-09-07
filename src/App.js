d4bc781ba5bdb1324a4fbfd5c0f41153ed621621
Giuseppe Valenti <giuseppevalenti@Giuseppes-MacBook-Pro-4.local>
2017-09-07 16:32:20 +0200

    * added bootstrap, react router, home and notFound containers, navbar with placeholders, 404 page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { Nav, Navbar } from "react-bootstrap";
+
+import "./App.css";
+
+import Routes from "./Routes";
+import RouteNavItem from "./components/RouteNavItem";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="App container">
+        <Navbar fluid collapseOnSelect>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <Link to="/REACT/">My Badges</Link>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+          <Nav pullRight>
+            <RouteNavItem href="/signup">Signup</RouteNavItem>
+            <RouteNavItem href="/login">Login</RouteNavItem>
+          </Nav>
+        </Navbar.Collapse>
+        </Navbar>
+        <Routes />
       </div>
     );
   }
---
+++ src/containers/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./Home.css";
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div className="Home">
+        <div className="lander">
+          <h1>My Badges</h1>
+          <p>A network for techies, their apps and tools..</p>
+        </div>
+      </div>
+    );
+  }
+}
